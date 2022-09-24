5e7ebb86134e4f4a0d0785b79c389c2baabb3e7d
allufsen <allufsensahala@gmail.com>
2022-09-24 16:53:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Transaction from "./components/Transaction";
+import SaldoBox from "./components/SaldoBox";
+import AddTransaction from "./components/AddTransaction";
 
-function App() {
+let url = "https://rupiahkuh-default-rtdb.";
+url += "asia-southeast1.firebasedatabase.app";
+url += "/transaction.json";
+
+const App = () => {
+  const [transactions, setTransaction] = useState([]);
+  const [submitCount, setSubmitCount] = useState(0);
+
+  // Use effect untuk mengakses data dari API firebase
+  useEffect(() => {
+    const myFetch = async () => {
+      try {
+        let response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        let responseData = await response.json();
+
+        const initTansactions = [];
+        for (const key in responseData) {
+          initTansactions.push({
+            id: key,
+            tanggal: responseData[key].tanggal,
+            keterangan: responseData[key].keterangan,
+            nominal: responseData[key].nominal,
+          });
+        }
+        console.log(JSON.stringify(initTansactions));
+        // atur ulang urutan transaction agar tanggal terkecil di bagian atas
+        initTansactions.sort((a, b) => a.tanggal - b.tanggal);
+
+        setTransaction(initTansactions);
+      } catch (error) {
+        console.log(`Terjadi gangguan dengan pesan: "${error}"`);
+      }
+    };
+    myFetch();
+  }, [submitCount]);
+
+  // handler untuk menambah data transaction,
+  // akan di-trigger dari komponen AddTransaction
+  const handleTambahTransaction = async (data) => {
+    // Kirim data ke server (firebase)
+    try {
+      let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      setSubmitCount(submitCount + 1);
+    } catch (error) {
+      console.log(`Terjadi gangguan dengan pesan: "${error}"`);
+    }
+  };
+
+  // handler untuk menghapus data transaction di komponen AddTransaction
+  const handleHapusTransaction = async (e) => {
+    // Rangkai alamat URL agar berisi id data yang dihapus
+
+    let url = "https://rupiahkuh-default-rtdb.";
+    url += "asia-southeast1.firebasedatabase.app";
+    url += "/transaction/";
+    url += `${e.target.id}.json`;
+
+    // Kirim delete request ke server (firebase)
+    try {
+      let response = await fetch(url, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      setSubmitCount(submitCount + 1);
+    } catch (error) {
+      console.log(`Terjadi gangguan dengan pesan: "${error}"`);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <SaldoBox transactions={transactions} />
+      <Transaction transactions={transactions} onHapusTransaction={handleHapusTransaction} />
+      <AddTransaction onTambahTransaction={handleTambahTransaction} />
+      <Footer />
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer id="footer" className="text-white py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col text-center">
+            <p className="lead mb-0">RUPIAH</p>
+            <p>Catat setiap pemasukan dan pengeluaranmu.
+              Belum terdaftar di OJK :)</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
